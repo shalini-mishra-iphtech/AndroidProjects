5c81dc73d5c2406ad5036673ed261cc1f3d9eb44
shalini-mishra-iphtech <shalini.mishra@iphtechnologies.com>
2025-02-12 15:36:51 +0530

    Add files via upload

---
+++ DataTypes.kt
@@ -0,0 +1,47 @@
+package com.example.lib3
+
+fun main() {
+    //1. Numbers
+    //2. Character
+    //3. Boolean
+    //4. String
+    //5. Arrays
+
+    /*1.Numbers-
+        Integer:-
+               Byte,Short,Int,Long
+        Floating:-
+                Float,Double
+     */
+
+    var myNum:Byte=45
+    println(myNum)
+    val myNum1:Short =5500
+    println(myNum1)
+    val myNum3 :Int=20456777
+    println(myNum3)
+    val myNum4 :Long=5066666634343L
+    println(myNum4)
+
+    //Floating
+    val f1=3.455F
+    println(f1)
+    val f2=23.333
+    println(f2)
+
+   //Character
+    val c1:Char='H'
+    println(c1)
+
+    //Boolean
+    val bool1:Boolean=true
+    println(bool1)
+    val bool2:Boolean=false
+    println(bool2)
+
+    //String
+    val myData="Hello World!"
+    println(myData)
+
+
+}
