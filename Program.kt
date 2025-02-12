e648e7c9ea55bbed5623561f7f0aa5509ad480be
shalini-mishra-iphtech <shalini.mishra@iphtechnologies.com>
2025-02-12 15:35:18 +0530

    Add files via upload

---
+++ Program.kt
@@ -0,0 +1,31 @@
+package com.example.lib2
+
+fun main(){
+    println("Hello World!");  //Single line Comments
+    println("I am learning Kotlin."); /*This is used
+                                        for multiline comments
+                                       */
+    println("It is awesome");
+    println(4+4)
+    print("Hello");
+    println("Coders")
+
+    //variable
+    val name:String="John"
+    val birthyear:Int=1975
+    println("User Name is "+name)
+    println(birthyear)
+
+    //var age=34
+    //println(age)
+
+    var age:Int
+    age=34
+    println(age)
+
+    val firstName="John "
+    val lastName="Doe"
+    val fullName=firstName+lastName
+    println("Student Full name is: " + fullName)
+
+}
