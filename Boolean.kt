8ca6aaf3678cccbd00901b6fc948b58df8d57e7e
shalini-mishra-iphtech <shalini.mishra@iphtechnologies.com>
2025-02-13 17:41:33 +0530

    Add files via upload

---
+++ Boolean.kt
@@ -0,0 +1,16 @@
+package com.example.lib7
+
+fun main(){
+    //Boolean
+    val isKotlinFun:Boolean=true
+    val isFishTasty:Boolean=false
+    println(isKotlinFun)
+    println(isFishTasty)
+
+    //We can use boolean with the coditional operator
+    println(10>9)
+    println(10==10)
+    println(7<4)
+    println(4>6)
+
+}
